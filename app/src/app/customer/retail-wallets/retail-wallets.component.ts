5eff3f166a5843a7abb8e28ed021818d5b71ffd4
Peter Tasner <petertasner@users.noreply.github.com>
2021-08-30 23:48:44 +0200

    Fix Retail-Wallet details

--- app/src/app/customer/retail-wallets/retail-wallets.component.ts
+++ app/src/app/customer/retail-wallets/retail-wallets.component.ts
@@ -1,94 +1,82 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import {
-  AssetTypes,
-  LockRetailWalletsDto,
-  RetailWalletDto,
-  RetailWalletDtoListApiResponse,
+    AssetTypes,
+    LockRetailWalletsDto,
+    RetailWalletDto,
+    RetailWalletDtoListApiResponse,
 } from '../../core/models';
 import { AssetTypeUtilityService } from '../../core/services/asset-types-utility.service';
 import { CustomApiService } from '../../core/services/ganymede.service';
 
 @Component({
-  selector: 'aap-retail-wallets',
-  templateUrl: './retail-wallets.component.html',
-  styleUrls: ['./retail-wallets.component.scss'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({ height: '0px', minHeight: '0' })),
-      state('expanded', style({ height: '*' })),
-      transition(
-        'expanded <=> collapsed',
-        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
-      ),
-    ]),
-  ],
+    selector: 'aap-retail-wallets',
+    templateUrl: './retail-wallets.component.html',
+    styleUrls: ['./retail-wallets.component.scss'],
+    animations: [
+        trigger('detailExpand', [
+            state('collapsed', style({ height: '0px', minHeight: '0' })),
+            state('expanded', style({ height: '*' })),
+            transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+        ]),
+    ],
 })
 export class RetailWalletsComponent implements OnInit {
-  displayedColumns: string[] = [
-    'assetType',
-    'publicAddress',
-    'nativeBalance',
-    'lockstatus',
-  ];
-  innerTableColums: string[] = ['token', 'balance'];
-  customerId: string;
-  wallets: RetailWalletDtoListApiResponse;
-  expandedElement: RetailWalletDto | null;
-  isLocked: boolean = false;
-  disableLock: boolean = false;
-  constructor(
-    private customApi: CustomApiService,
-    private assetTypeUtility: AssetTypeUtilityService,
-    private route: ActivatedRoute
-  ) {}
+   
+    displayedColumns: string[] = ['assetType', 'publicAddress', 'nativeBalance', 'lockstatus'];
+    innerTableColums: string[] = ['token', 'balance'];
+    customerId: string;
+    wallets: RetailWalletDtoListApiResponse;
+    expandedElement: RetailWalletDto | null;
+    isLocked: boolean = false;
+    disableLock: boolean = false;
 
-  ngOnInit(): void {
-    this.customerId = this.route.snapshot.paramMap.get('customer-id');
-    this.getRetailWallets();
-  }
+    AssetTypes = AssetTypes;
 
-  getRetailWallets() {
-    this.customApi
-      .getRetailWalletGetRetailWalletsDetails(this.customerId)
-      .subscribe(
-        (data) => {
-          this.wallets = data;
-          if (this.wallets?.data?.length > 0) {
-            this.isLocked = this.wallets.data[0].seedLockStatus;
-          }
-        },
-        () => {},
-        () => {
-          this.disableLock = false;
-        }
-      );
-  }
+    constructor(
+        private customApi: CustomApiService,
+        private assetTypeUtility: AssetTypeUtilityService,
+        private route: ActivatedRoute
+    ) {}
 
-  lockWallet(lock: boolean) {
-    this.disableLock = true;
-    const dto = new LockRetailWalletsDto();
-    dto.lock = lock;
-    this.customApi
-      .putRetailWalletLockRetailWallets(this.customerId, dto)
-      .subscribe((data) => {
-        if (data?.data) {
-          this.getRetailWallets();
-        }
-      });
-  }
+    ngOnInit(): void {
+        this.customerId = this.route.snapshot.paramMap.get('customer-id');
+        this.getRetailWallets();
+    }
 
-  public getIcon(assetType: string): string {
-    var at: AssetTypes = AssetTypes[assetType];
-    var icon = this.assetTypeUtility.icon(at);
-    return icon;
-  }
+    getRetailWallets() {
+        this.customApi.getRetailWalletGetRetailWalletsDetails(this.customerId).subscribe(
+            (data) => {
+                this.wallets = data;
+                if (this.wallets?.data?.length > 0) {
+                    this.isLocked = this.wallets.data[0].seedLockStatus;
+                }
+            },
+            () => {},
+            () => {
+                this.disableLock = false;
+            }
+        );
+    }
+
+    lockWallet(lock: boolean) {
+        this.disableLock = true;
+        const dto = new LockRetailWalletsDto();
+        dto.lock = lock;
+        this.customApi.putRetailWalletLockRetailWallets(this.customerId, dto).subscribe((data) => {
+            if (data?.data) {
+                this.getRetailWallets();
+            }
+        });
+    }
+
+    getIcon(assetType: AssetTypes): string {
+        return this.assetTypeUtility.icon(assetType);
+    }
+
+    getUnit(assetType: AssetTypes): string {      
+      return this.assetTypeUtility.unit(assetType);
+    }
 }
