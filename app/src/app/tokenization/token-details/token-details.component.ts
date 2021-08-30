b6e0997f1e537a225f60c870f24f2bd0908f197f
Peter Tasner <petertasner@users.noreply.github.com>
2021-08-30 23:29:42 +0200

    Displaying token details

--- app/src/app/tokenization/token-details/token-details.component.ts
+++ app/src/app/tokenization/token-details/token-details.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { TokenizedAssetDetailsDto } from '../../core/models';
+import { AssetTypes, TokenizedAssetDetailsDto } from '../../core/models';
+import { AssetTypeUtilityService } from '../../core/services/asset-types-utility.service';
 import { CustomApiService } from '../../core/services/ganymede.service';
+
+interface KeyValue {
+    key: string;
+    value: object;
+}
 
 @Component({
     selector: 'app-token-details',
@@ -10,15 +16,20 @@ import { CustomApiService } from '../../core/services/ganymede.service';
     styleUrls: ['./token-details.component.scss'],
 })
 export class TokenDetailsComponent implements OnInit {
+    tokenDetailsColumns = ['key', 'value'];
+    transferColumns = ['created', 'txId', 'fromAddress', 'toAddress', 'amount'];
+    optInColumns = [];
 
     issuerWalletSeedId: string;
     issuerWalletId: string;
     tokenizedAssetId: string;
     tokenizedAsset: TokenizedAssetDetailsDto;
+    tokenDetails: KeyValue[];
 
     constructor(
         private customApi: CustomApiService,
         private activatedRoute: ActivatedRoute,
+        private assetTypeUtility: AssetTypeUtilityService,
         private router: Router
     ) {}
 
@@ -32,9 +43,12 @@ export class TokenDetailsComponent implements OnInit {
         });
     }
 
-    sendTokens(){
-      
+    showInBlockchainExplorer(txId: string) {
+        const url = `${this.assetTypeUtility.txUrl(AssetTypes.XLM)}/${txId}`;
+        window.open(url, '_blank');
     }
+
+    sendTokens() {}
 
     private loadTokenDetails() {
         this.customApi
@@ -45,6 +59,24 @@ export class TokenDetailsComponent implements OnInit {
             )
             .subscribe((response) => {
                 this.tokenizedAsset = response.data;
+                this.tokenDetails = this.getTokenDetails(response.data);
             });
+    }
+
+    private getTokenDetails(tokenizedAsset: TokenizedAssetDetailsDto): KeyValue[] {
+        const relevantProps = ['assetId', 'name', 'unitName', 'totalSupply', 'decimals', 'url'];
+        const details: KeyValue[] = [];
+
+        for (const prop in this.tokenizedAsset) {
+            if (
+                Object.prototype.hasOwnProperty.call(tokenizedAsset, prop) &&
+                relevantProps.indexOf(prop) !== -1
+            ) {
+                const element = this.tokenizedAsset[prop];
+                details.push({ key: prop, value: element });
+            }
+        }
+
+        return details;
     }
 }
