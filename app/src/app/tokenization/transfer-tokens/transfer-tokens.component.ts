76ea522eeb8bf563db568727843057f0133b8217
Adnan <a.inayat@bloxxon.co>
2021-09-16 16:55:03 +0200

    added CSV view table

--- app/src/app/tokenization/transfer-tokens/transfer-tokens.component.ts
+++ app/src/app/tokenization/transfer-tokens/transfer-tokens.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { Form, FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { CustomApiService } from 'src/app/core/services/ganymede.service';
 
 import {
@@ -24,11 +26,21 @@ export class TransferTokensComponent implements OnInit {
     issuerWalletSeedId: string;
     issuerWalletId: string;
     tokenizedAssetId: string;
-    amount: number;
+
     selectedTab: FormControl;
+
+    amount: number;
     selectedWallet: string;
+
     selectedFile: File;
     selectedFileName: string;
+
+    dataSource: MatTableDataSource<TokenizedAssetToRetailWallet>;
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild('credForm') credForm;
+
+    csvColumns = ['RetailWalletId', 'Amount'];
 
     wallets: RetailWallet[] = [
         { value: '1ddaa5e4-747c-4283-aa33-e965d422f45f', viewValue: 'Wallet 1' },
@@ -52,9 +64,9 @@ export class TransferTokensComponent implements OnInit {
             this.model = new IssuerWalletTokenizedAssetTransferDto({
                 credentials: new SimpleAccessCredentialsDto(),
             });
+
         });
     }
-
     submit() {
         if (this.selectedTab.value === 0) { //Single transfer
             this.model.transfers = new Array<TokenizedAssetToRetailWallet>();
@@ -64,26 +76,42 @@ export class TransferTokensComponent implements OnInit {
             }));
             this.customApi.postTokenizedAssetsTransferTokenizedAssets(this.issuerWalletSeedId, this.issuerWalletId, this.tokenizedAssetId, this.model)
                 .subscribe((response) => {
+                    this.resetFields();
+                },
+                () => {
+                    this.resetFields();
                 });
         } else {  //Multiple transfer
-            this.ngxCsvParser.parse(this.selectedFile, { header: false, delimiter: ',' })
-                .pipe().subscribe((result: Array<any>) => {
-                    this.model.transfers = new Array<TokenizedAssetToRetailWallet>();
-                    for (let i = 0; i < result.length; i++)
-                        this.model.transfers.push(new TokenizedAssetToRetailWallet({
-                            amount: result[i][1],
-                            retailWalletId: result[i][0]
-                        }));
-                    this.customApi.postTokenizedAssetsTransferTokenizedAssets(this.issuerWalletSeedId, this.issuerWalletId, this.tokenizedAssetId, this.model)
-                        .subscribe((response) => {
-                        });
-                }, (error: NgxCSVParserError) => {
-                    console.log('Error', error);
+            this.customApi.postTokenizedAssetsTransferTokenizedAssets(this.issuerWalletSeedId, this.issuerWalletId, this.tokenizedAssetId, this.model)
+                .subscribe((response) => {
+                    this.resetFields();
+                },
+                () => { 
+                    this.resetFields();
                 });
         }
     }
     csvInputChange(fileInputEvent: any) {
         this.selectedFile = fileInputEvent.target.files[0];
         this.selectedFileName = this.selectedFile.name;
+        this.ngxCsvParser.parse(this.selectedFile, { header: false, delimiter: ',' })
+            .pipe().subscribe((result: Array<any>) => {
+                this.model.transfers = new Array<TokenizedAssetToRetailWallet>();
+                for (let i = 0; i < result.length; i++)
+                    this.model.transfers.push(new TokenizedAssetToRetailWallet({
+                        amount: result[i][1],
+                        retailWalletId: result[i][0]
+                    }));
+                this.dataSource = new MatTableDataSource<TokenizedAssetToRetailWallet>(this.model.transfers);
+                this.dataSource.paginator = this.paginator;
+            }, (error: NgxCSVParserError) => {
+                console.log('Error', error);
+            });
+    }
+    resetFields() {
+        this.dataSource = new MatTableDataSource<TokenizedAssetToRetailWallet>();
+        this.model.transfers = new Array<TokenizedAssetToRetailWallet>();
+        this.selectedFile = null;
+        this.selectedFileName = '';
     }
 }
