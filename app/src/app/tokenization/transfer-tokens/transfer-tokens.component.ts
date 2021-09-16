aea7882a63c425cbf7b4552de37297ed9231e452
Adnan <a.inayat@bloxxon.co>
2021-09-16 15:04:33 +0200

    Added transfer token component

--- app/src/app/tokenization/transfer-tokens/transfer-tokens.component.ts
+++ app/src/app/tokenization/transfer-tokens/transfer-tokens.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { NgxCsvParser, NgxCSVParserError } from 'ngx-csv-parser';
+import { CustomApiService } from 'src/app/core/services/ganymede.service';
 
 import {
     IssuerWalletTokenizedAssetTransferDto,
     SimpleAccessCredentialsDto,
+    TokenizedAssetToRetailWallet,
 } from '../../core/models';
+
+interface RetailWallet {
+    value: string;
+    viewValue: string;
+}
 
 @Component({
     selector: 'app-transfer-tokens',
@@ -15,12 +24,27 @@ export class TransferTokensComponent implements OnInit {
     issuerWalletSeedId: string;
     issuerWalletId: string;
     tokenizedAssetId: string;
+    amount: number;
+    selectedTab: FormControl;
+    selectedWallet: string;
+    selectedFile: File;
+    selectedFileName: string;
+
+    wallets: RetailWallet[] = [
+        { value: '1ddaa5e4-747c-4283-aa33-e965d422f45f', viewValue: 'Wallet 1' },
+        { value: '2ddaa5e4-747c-4283-aa33-e965d422f45f', viewValue: 'Wallet 2' },
+        { value: '3ddaa5e4-747c-4283-aa33-e965d422f45f', viewValue: 'Wallet 3' }
+    ];
 
     model: IssuerWalletTokenizedAssetTransferDto;
 
-    constructor(private activatedRoute: ActivatedRoute) {}
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private ngxCsvParser: NgxCsvParser,
+        private customApi: CustomApiService) { }
 
     ngOnInit(): void {
+        this.selectedTab = new FormControl(0);
         this.activatedRoute.params.subscribe((params) => {
             this.issuerWalletSeedId = params.seedId;
             this.issuerWalletId = params.issuerWalletId;
@@ -31,5 +55,35 @@ export class TransferTokensComponent implements OnInit {
         });
     }
 
-    submit() {}
+    submit() {
+        if (this.selectedTab.value === 0) { //Single transfer
+            this.model.transfers = new Array<TokenizedAssetToRetailWallet>();
+            this.model.transfers.push(new TokenizedAssetToRetailWallet({
+                amount: this.amount,
+                retailWalletId: this.selectedWallet
+            }));
+            this.customApi.postTokenizedAssetsTransferTokenizedAssets(this.issuerWalletSeedId, this.issuerWalletId, this.tokenizedAssetId, this.model)
+                .subscribe((response) => {
+                });
+        } else {  //Multiple transfer
+            this.ngxCsvParser.parse(this.selectedFile, { header: false, delimiter: ',' })
+                .pipe().subscribe((result: Array<any>) => {
+                    this.model.transfers = new Array<TokenizedAssetToRetailWallet>();
+                    for (let i = 0; i < result.length; i++)
+                        this.model.transfers.push(new TokenizedAssetToRetailWallet({
+                            amount: result[i][1],
+                            retailWalletId: result[i][0]
+                        }));
+                    this.customApi.postTokenizedAssetsTransferTokenizedAssets(this.issuerWalletSeedId, this.issuerWalletId, this.tokenizedAssetId, this.model)
+                        .subscribe((response) => {
+                        });
+                }, (error: NgxCSVParserError) => {
+                    console.log('Error', error);
+                });
+        }
+    }
+    csvInputChange(fileInputEvent: any) {
+        this.selectedFile = fileInputEvent.target.files[0];
+        this.selectedFileName = this.selectedFile.name;
+    }
 }
